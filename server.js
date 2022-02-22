b5b8f5e1debaba84943cf209a1e86893c32c97d4
vijaylpersad <vijaylpersad@utexas.edu>
2022-02-21 20:19:46 -0600

    results object shows up in console, need to display on page

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
+const axios = require('axios')
 const express = require('express');
+require('dotenv').config()
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 
@@ -17,8 +19,23 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  //res.send('Hello, backend!');
+  res.render('index.ejs')
 });
+
+
+//GET request --use query strings -- req.query
+app.get('/results', (req,res)=>{
+    
+  const url = `http://www.omdbapi.com/?t=${req.query.q}&apikey=${process.env.OMDB_API_KEY}`
+  console.log(url)
+
+  axios.get(url)
+  .then(response => {
+    console.log(response)   // works to see movie details in console 
+    res.render('results.ejs', results)
+  })
+})
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
