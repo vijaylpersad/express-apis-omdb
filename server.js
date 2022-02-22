767a3fd24d8393dfe872ad0a76f9e143f27fe0eb
vijaylpersad <vijaylpersad@utexas.edu>
2022-02-22 10:59:41 -0600

    rewatch axios lecture

--- server.js
+++ server.js
@@ -32,9 +32,19 @@ app.get('/results', (req,res)=>{
 
   axios.get(url)
   .then(response => {
-    console.log(response)   // works to see movie details in console 
-    res.render('results.ejs', results)
+    console.log(response.data)   // works to see movie details in console 
+    //res.render('results.ejs')
+
+    const searchResults = response.data
+    res.render('results.ejs', { results: searchResults }) //Object.entries returns an array. ejs templates will print contents of array
+
   })
+})
+
+
+//In the results from the API, we notice that in every movie entry there is a IMDBid. In the rendered HTML for /results, have each movie link to a route like /movies/tt234323 (where tt234323 is the IMDBid for that movie)
+app.get("/movies/:movie_id", (req, res) =>{
+
 })
 
 // The app.listen function returns a server handle
